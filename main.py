30f4583b1d07df0b80fd75d80310b0bd72391347
Voblog <stefan.webster999@gmail.com>
2022-02-21 18:40:57 -0600

    refactered added cheatmode ha ha ha ha ha
    Co-authored-by: Casey Webster <casey@wxster.com>

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from random import seed
 from random import randint
 
+def gamerun(max, cheatmode=False):
+    print("the game is starting yay")
+    nguesses=0
+    n= randint(0,max)
+    min=0
+    while True:
+        # ask for a number
+        if cheatmode:
+            print(f"({min} - {max}) ", end='')
+        guess=input("guess a number: ")
+        g =int(guess)
+        nguesses+=1
+        # check if it is right or not
+        if g==n:
+            print(f"correct! yay it took you {nguesses} trys")
+            break
+        elif g > n:
+            print("too high")
+            if g <max:
+                max=g
+        else:
+            print("too too low")
+            if g >min:
+                min=g
+
 
 print("welcome to the guessing game by voblog")
 
@@ -9,16 +34,4 @@ print(f"hello {name}!")
 
 # pick a random number
 seed()
-n= randint(0,1000)
-while True:
-    # ask for a number
-    guess=input("guess a number: ")
-    g =int(guess)
-    # check if it is right or not
-    if g==n:
-        print("correct! yay")
-        break
-    elif g > n:
-        print("too high")
-    else:
-        print("too too low")
+gamerun(1000,True)
